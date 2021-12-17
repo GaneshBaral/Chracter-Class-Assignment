85b49e44ad625fe03428647150843fecdd828ea0
GaneshBaral <66331075+GaneshBaral@users.noreply.github.com>
2021-12-16 19:05:19 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+#Character class assignment - Ganesh Baral 
+
+class Character:
+    def __init__(self, name, phrase1, phrase2,):
+        self.name = name
+        self.phrase1 = phrase1
+        self.phrase2 = phrase2
+        self.level = 0
+
+    def speak(self, phraseNum):
+        if phraseNum == 1:
+            print(self.phrase1)
+        elif phraseNum == 2:
+            print(self.phrase2)
+
+    def setLevel(self, newLevel):
+        self.level = newLevel
+        print("Level: " + str(self.level))
+
+
+# Character Objects
+JohnWick = Character("John Wick", "IT WASN'T JUST A PUPPY.", "YOU WANTED ME BACK. I'M BACK.")
+Thanos = Character("Thanos", "I AM INEVITABLE.", "PERFECLY BALANCED, AS ALL THINGS SHOULD BE.")
+
+
+# Object Method Call
+JohnWick.speak(1)
+Thanos.setLevel(2) 
+Thanos.speak(2)
+
+
